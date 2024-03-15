c3e6741b1d8d5944d2e97a8f8ccde1c2f3cdeaf9
полина <PDBoeva@stud.kantiana.ru>
2024-03-15 15:01:05 +0200

    -

--- task1/check_contact.php
+++ task1/check_contact.php
@@ -5,35 +5,48 @@
     unset($_SESSION["success"]);
     unset($_SESSION["error_file"]);
     function redirect(){
-        header('Location: main.php');
+        header("Location: main.php");
         exit;
     }
+ 
+    function uploadpic(){
+        $allowed = array("png", "jpg", "");
+        $target_dir = "uploads/"; 
+        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+        $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                if (!in_array($file_type, $allowed)) {
+                    $_SESSION["error_file"] = "Недопустимое разрешение файла";
+                } else {
+                    move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
+                }
+    }
 
-    $username = htmlspecialchars(trim($_POST['username']));
-    $email = htmlspecialchars(trim($_POST['email']));
-    $message = htmlspecialchars(trim($_POST['message']));
-    $filepath = $_FILES["file"]["tmp_name"]; 
-    $filename = $_FILES['file']['name'];
-    $filetype = pathinfo($filename, PATHINFO_EXTENSION);
-    $allowed = array('png', 'jpg', '');
+    $username = htmlspecialchars(trim($_POST["username"]));
+    $email = htmlspecialchars(trim($_POST["email"]));
+    $message = htmlspecialchars(trim($_POST["message"]));
+    $filename = $_FILES["file"]["name"];
+
 
     if(strlen($username) == 0 || strlen($email) == 0 || strlen($message) == 0){
-        $_SESSION['error_message'] = 'Заполните все обязательные поля';  
+        $_SESSION["error_message"] = "Заполните все обязательные поля";  
         redirect();
     }
     else if(strpos($email, "@") == false){
-        $_SESSION['error_email'] = 'Укажите верную электронную почту';
+        $_SESSION["error_email"] = "Укажите верную электронную почту";
         redirect();
     }
-    else if(!in_array($filetype, $allowed)){
-        $_SESSION['error_file'] = 'Недопустимое разрешение файла';
-        echo $filetype;
-        //redirect();
-    }
     else{
-        $_SESSION['success'] = "Ваше сообщение отправлено!";
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["file"])) {
+            if(is_dir($target_dir)) {
+                uploadpic();
+            } else {
+                mkdir("uploads/");
+                uploadpic();
+            }
+        }
+        $_SESSION["success"] = "Ваше сообщение отправлено!";
         $f = fopen("$username.txt", "w");
-        fwrite($f, $username. "\t".$email. "\t".$message. "\t".$filepath); 
+        fwrite($f, $username. "\n".$email."\n".$message."\n".$_SERVER['DOCUMENT_ROOT']."/".basename(__DIR__)."/"."uploads/".basename($_FILES["file"]["name"])); 
         fclose($f);
         redirect();
     }
